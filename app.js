f5a5de77bcf9daf754fd8efa4188a94678963cf7
mbilalmalik <bilal.malik@mercurialminds.com.pk>
2018-05-21 14:18:31 +0500

    adding parse bridge app

---
+++ app.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var parse = require('./parse');
+
+var http = require('http');
+var debug = require('debug')('project:server');
+
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// single parse api bridge for complaints
+app.use('/', parse);
+
+// region HANDLERS
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+// endregion
+
+// region bin/www
+var port = normalizePort(process.env.PORT || '2210');
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+// region HELPERS
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+// endregion
+
+// endregion
+
+module.exports = app;
---
+++ parse.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+
+const PARSE_HTTP_OPTIONS = {
+    hostname: 'mm-dashboard.herokuapp.com',
+    // hostname: 'localhost',
+    port: 80,
+    path: '/parse/classes/Complaint',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        // "Content-Length": Buffer.byteLength(data),
+        'X-Parse-Application-Id': 'myAppId'
+    }
+};
+
+function validateComplaintParams(cellNumber, dealershipNameLocation, bookingNumber, userComplain, userComplainNumber, userErrorResponse) {
+    return {
+        cellNumber: cellNumber ? cellNumber : 'N/A',
+        dealershipNameLocation: dealershipNameLocation ? dealershipNameLocation : 'N/A',
+        bookingNumber: bookingNumber ? bookingNumber : 'N/A',
+        userComplain: userComplain ? userComplain : 'N/A',
+        userErrorResponse: userErrorResponse ? userErrorResponse : 'N/A',
+        userComplainNumber: userComplainNumber ? userComplainNumber : 'N/A'
+    }
+}
+
+function postRequestParams(body) {
+    var cellNumber = body.cellNumber;
+    var dealershipNameLocation = body.dealershipNameLocation;
+    var bookingNumber = body.bookingNumber;
+    var userComplain = body.userComplain;
+    var userErrorResponse = body.userErrorResponse;
+    var userComplainNumber = body.userComplainNumber;
+
+    return validateComplaintParams(cellNumber, dealershipNameLocation, bookingNumber, userComplain, userComplainNumber, userErrorResponse);
+}
+
+function getRequestParams(params) {
+	var userName = params.userName;
+    var cellNumber = params.cellNumber;
+    var dealershipNameLocation = params.dealershipNameLocation;
+    var bookingNumber = params.bookingNumber;
+    var userComplain = params.userComplain;
+    var userErrorResponse = params.userErrorResponse;
+    var userComplainNumber = params.userComplainNumber;
+
+    return validateComplaintParams(cellNumber, dealershipNameLocation, bookingNumber, userComplain, userComplainNumber, userErrorResponse);
+}
+
+function postParseComplaints(params, res, isGet) {
+    var data;
+    if (isGet) {
+        PARSE_HTTP_OPTIONS.method = "GET";
+        if (params)
+            data = querystring.stringify(params);
+    } else {
+        PARSE_HTTP_OPTIONS.method = "POST";
+        data = JSON.stringify(params);
+        PARSE_HTTP_OPTIONS.headers.Content_Length = Buffer.byteLength(data);
+    }
+
+    var callback = function (response) {
+        var str = '';
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        response.on('end', function () {
+            console.log(str);
+            res.json({status: true, data: JSON.parse(str)});
+        });
+    };
+
+    var req = http.request(PARSE_HTTP_OPTIONS, callback);
+    req.on('error', function (err) {
+        console.log('problem with request: ' + err.message);
+        res.json({status: false, message: err});
+    });
+
+    if (!isGet)
+        req.write(data);
+
+    req.end();
+}
+
+function postToParse(req, res, isGet) {
+    var params;
+    if (isGet) {
+        var temp = url.parse(req.url, true).query;
+        params = getRequestParams(temp);
+    } else {
+		
+        params = postRequestParams(req.body);
+    }
+    postParseComplaints(params, res, false);
+}
+
+router.get('/', function (req, res, next) {
+    res.json({message: 'server is up and running'})
+});
+
+router.get('/parse/save-complaint',
+    function (req, res, next) {
+        postToParse(req, res, true);
+    });
+
+router.post('/parse/save-order', function (req, res, next) {
+    postToParse(req, res, false);
+});
+
+router.route('/parse/get-complaints')
+    .get(function (req, res, next) {
+        postParseComplaints(null, res, true);
+    });
+
+module.exports = router;
